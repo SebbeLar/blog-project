4ef6d33ee7911c8766a7e69159902518634b64ee
Sebastian Larsson <sebberlarsson@hotmail.com>
2016-11-18 14:55:45 +0100

    Started with the landing page

--- src/components/LandingPage/landingPage.js
+++ src/components/LandingPage/landingPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import styles from './landingPage.scss';
 
 const LandingPage = () => {
     return (
-        <div>Landing Page!</div>
+        <div className={styles.container}>
+            <h1 className={styles.headerName}>Sebastian Larsson</h1>
+            <h3 className={styles.headerTitle}>Front end developer</h3>
+            <a className={styles.headerButton}>Continue</a>
+        </div>
     );
 };
 
